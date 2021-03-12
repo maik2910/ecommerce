e42084b95e78a7e7bf48c65d4ff0f67d8772c408
Maikdouglas Ferreira <maikdouglas@hiperideal.com.br>
2021-03-12 14:39:27 -0300

    criação do pageadmin e tpl admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 //namespace
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 //rotas
 $app = new Slim();
@@ -13,6 +14,14 @@ $app->config('debug', true);
 $app->get('/', function() {
     
     $page = new Page();
+
+    $page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+    
+    $page = new PageAdmin();
 
     $page->setTpl("index");
 
