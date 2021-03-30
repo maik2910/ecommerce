94825f09e8f8c31d6b5e70704c9dcb7319b90794
Maikdouglas Ferreira <maikdouglas@hiperideal.com.br>
2021-03-30 08:26:34 -0300

    compras frete

--- site.php
+++ site.php
@@ -67,7 +67,9 @@ $app->get("/cart", function(){
 	$page = new Page();
 	$page->setTpl("cart", [
 		'cart'=>$cart->getValues(),
-		'products'=>$cart->getProducts()]);
+		'products'=>$cart->getProducts(),
+		'error'=>Cart::getMsgError()
+	]);
 });
 
 $app->get("/cart/:idproduct/add", function($idproduct){
@@ -119,6 +121,17 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 	header("Location: /cart");
 	exit;
 
+});
+
+$app->post("/cart/freight", function(){
+
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST['zipcode']);
+
+	header("Location: /cart");
+	exit;
+	
 });
 
 ?>
