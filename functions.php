877a3b25040b549efe67a9c3d3aeed17ec12cbae
Maikdouglas Ferreira <maikdouglas@hiperideal.com.br>
2021-09-03 13:50:29 -0300

    DETALHES DO PEDIDO FATURADO

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 	use \Hcode\Model\User;
+	use \Hcode\Model\Cart;
+	
 	function formatPrice($vlprice)
 	{
 		if ($vlprice < 0) $vlprice = 0;
@@ -16,4 +18,19 @@
 		$user=User::getFromSession();
 		return $user->getdesperson();
 	}
+
+	function getCartNrQtd()
+	{
+	$cart =  Cart::getFromSession();
+	$totals = $cart->getProductsTotals();
+	return $totals['nrqtd'];
+	}
+
+	function getCartVlSubtotal()
+	{
+	$cart =  Cart::getFromSession();
+	$totals = $cart->getProductsTotals();
+	return formatPrice($totals['vlprice']);
+	}
+?>
 ?>
